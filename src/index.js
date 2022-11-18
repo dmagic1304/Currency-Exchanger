89a3b312b1564db6e67545efd3921096bd3b90e8
Dominik Magic <dominikmagic5@gmail.com>
2022-11-18 11:50:38 -0800

    It will display an error message if currency that is not on the list is selected

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import ExchangeService from "./js/currency-exchange-service.js";
 
-// Business Logic
 function getExchange() {
   ExchangeService.getExchange()
     .then(function(response) {
@@ -15,13 +14,17 @@ function getExchange() {
     });
 }
 
-//UI Logic
 function printResults(apiData) {
   const displayParagraph = document.getElementById('result');
   const enteredAmount = document.getElementById('usd').value;
   const selectedCurrency = document.getElementById('exchange-option').value;
   const exchangeRate = apiData.conversion_rates[selectedCurrency];
+  displayParagraph.innerHTML = null;
+  if (exchangeRate) {
   displayParagraph.append(`Current exchange rate for ${selectedCurrency} is ${exchangeRate}. For ${enteredAmount} USD you will get ${enteredAmount * exchangeRate} ${selectedCurrency} `);
+  } else {
+    displayParagraph.append(`Unable to get data for selected currency (${selectedCurrency}). Please make sure to select a valid currency from the list!`);
+  }
 }
 
 function printError(apiError) {
