bb69120ccda843100743f2128646db986aac5fce
Dominik Magic <dominikmagic5@gmail.com>
2022-11-18 10:29:28 -0800

    added a basic printResults function to index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import ExchangeService from "./js/currency-exchange-service.js";
 
+// Business Logic
 function getExchange() {
   ExchangeService.getExchange()
     .then(function(response) {
@@ -12,5 +13,14 @@ function getExchange() {
         printError(response);
       }
     });
+}
+
+//UI Logic
+function printResults(apiData) {
+  const ul = document.querySelector('ul');
+  let li = document.createElement('li');
+  const selection = document.getElementById('exchange-option').value;
+  li.append(`It will be ${apiData.conversion_rates[selection]}`);
+  ul.append(li);
 }
 
