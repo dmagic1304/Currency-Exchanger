3bd5164fb4def54fd4e69cebd68d0538d6caf90d
Dominik Magic <dominikmagic5@gmail.com>
2022-11-18 09:54:04 -0800

    added getExchange() to index.js that will use ExchangeServices() to fetch data and call print functions

--- src/index.js
+++ src/index.js
@@ -3,4 +3,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import ExchangeService from "./js/currency-exchange-service.js";
 
-function getExchange()
+function getExchange() {
+  ExchangeService.getExchange()
+    .then(function(response) {
+      if (response.result === 'success') {
+        printResults(response);
+      } else {
+        printError(response);
+      }
+    });
+}
+
