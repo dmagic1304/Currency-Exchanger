3b67ab188c7b2b9dc3f861561a9e78493067d4dd
Dominik Magic <dominikmagic5@gmail.com>
2022-11-18 09:48:46 -0800

    adjusted ExchangeService() to not require any arguments since input is always in USD

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+import ExchangeService from "./js/currency-exchange-service.js";
+
+function getExchange()
--- src/js/currency-exchange-service.js
+++ src/js/currency-exchange-service.js
@@ -1,6 +1,6 @@
 export default class ExchangeService {
-  static getExchange(currency) {
-    return fetch(`GET https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${currency}`)
+  static getExchange() {
+    return fetch(`GET https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
       .then(function(response) {
         if (!response.ok) {
           const errorMessage = `${response.status} ${response.text}`;
