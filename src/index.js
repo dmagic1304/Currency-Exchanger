ea068b5ff763f79992ed20201d9a5e8105035776
Dominik Magic <dominikmagic5@gmail.com>
2022-11-18 17:12:00 -0800

    It will successfully change conversion direction once the button is clicked

--- src/index.js
+++ src/index.js
@@ -38,6 +38,23 @@ function printResults() {
   }
 }
 
+function printForeignResults() {
+  const rateParagraph = document.getElementById('rate');
+  const totalParagraph = document.getElementById('total');
+  const enteredAmount = document.getElementById('usd').value;
+  const selectedCurrency = document.getElementById('exchange-option').value;
+  const exchangeRate = sessionStorage.getItem(selectedCurrency);
+  rateParagraph.innerHTML = null;
+  totalParagraph.innerHTML= null;
+
+  if (exchangeRate) {
+    rateParagraph.append(`Current exchange rate for ${selectedCurrency} is ${exchangeRate}`);
+    totalParagraph.append(`For ${enteredAmount} ${selectedCurrency} you will get $${(enteredAmount / exchangeRate).toFixed(2)} USD`);
+  } else {
+    rateParagraph.append(`Selected currency does not exist (${selectedCurrency}). Please make sure to select a valid currency from the list!`);
+  }
+}
+
 function printError(apiError) {
   const rateParagraph = document.getElementById('rate');
   rateParagraph.append(`Not able to get requested data due to: ${apiError}`);
@@ -45,11 +62,15 @@ function printError(apiError) {
 
 function handleSubmit(e) {
   e.preventDefault();
+  const button = document.getElementById('switch').value;
+  console.log('test' + button);
   const rateParagraph = document.getElementById('rate');
   const totalParagraph = document.getElementById('total');
   const enteredAmount = document.getElementById('usd').value;
-  if (enteredAmount > 0) {
+  if (enteredAmount > 0 && button === '0') {
     printResults();
+  } else if (enteredAmount > 0 && button === '1') {
+    printForeignResults();
   } else {    
     rateParagraph.innerHTML = null;    
     totalParagraph.innerHTML= null;
--- src/js/exchange-direction.js
+++ src/js/exchange-direction.js
@@ -3,10 +3,10 @@ export default function directionSwitch() {
   if (button.innerText === 'Convert foreign currency to USD') {
     button.innerText = 'Convert USD to foreign currency';
     button.setAttribute('value', 1);
-    document.getElementById('input-label').innerText = 'Enter the amount in foreign currency:'
+    document.getElementById('input-label').innerText = 'Enter the amount in foreign currency:';
   } else {
     button.innerText = 'Convert foreign currency to USD';
     button.setAttribute('value', 0);
-    document.getElementById('input-label').innerText = 'Enter the amount in USD $'
+    document.getElementById('input-label').innerText = 'Enter the amount in USD $';
   }
 }
